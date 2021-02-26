5f02d8a1222ad68a5b7746c3eefecea1383c5788
Бурачков Александр1 <BurachkovAA1@ssk63.ru>
2021-02-26 15:28:15 +0400

    completed all tasks

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,42 @@
+'use strict'
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+	let x = [];
+	let discriminant = b ** 2  - 4 * a * c;
+	if(discriminant === 0) {
+		x.push(-b / (2 * a))
+	}
+	else if(discriminant > 0) {
+		x.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		x.push((-b - Math.sqrt(discriminant)) / (2 * a));
+	}
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+	let averageMark = 0;	
+	if (marks.length > 5) {
+		console.log('Количество оценок больше 5');
+		marks.splice(5);
+	}
+	if (marks.length !== 0) {
+		let sumMark = 0;
+		for (let i = 0; i < marks.length; i++) {
+			sumMark += marks[i];	
+		}
+		averageMark = sumMark / marks.length;
+	}
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let year = dateOfBirthday.getFullYear();
+	let today = (new Date()).getFullYear();
+	let result;
+	if ((today - year) >= 18) {
+		result = `Не желаете ли олд-фэшн, ${name}?`;
+	}
+	else {
+		result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+	}
+    return result;
 }
