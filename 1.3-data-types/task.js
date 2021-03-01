5e7442431714c441bd6f156509ebd376098ed88a
Бурачков Александр1 <BurachkovAA1@ssk63.ru>
2021-03-01 16:52:09 +0400

    Набросал решение Задачи №1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,28 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let totalAmount;
+    if (isNaN(parseInt(percent))) {
+        return `Параметр percent содержит неправильное значение "${percent}"`;
+    }
+    else if (isNaN(parseInt(contribution))) {
+        return `Параметр contribution содержит неправильное значение "${contribution}"`;
+    }
+    else if (isNaN(parseInt(amount))) {
+        return `Параметр amount содержит неправильное значение "${amount}"`;
+    }
+    else {
+        percent = parseInt(percent) / 1200;
+        contribution = parseInt(contribution);
+        amount = parseInt(amount);
+        let capital = amount - contribution;
+        let today = new Date();
+        let period = (date.getFullYear() - today.getFullYear()) * 12 - (date.getMonth() - today.getMonth());
+        let monthlyPay = capital * (percent + percent / (((1 + percent) ** period) - 1));
+        totalAmount = ((monthlyPay * period).toFixed(2));
+        totalAmount = String(totalAmount);
+        console.log(totalAmount);
+    }
+    return totalAmount;
 }
 
 function getGreeting(name) {
