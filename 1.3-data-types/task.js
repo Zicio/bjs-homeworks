5b19f94a416b8a2a63333afd01b39c2cf9856cbc
Бурачков Александр1 <BurachkovAA1@ssk63.ru>
2021-03-02 16:05:04 +0400

    Task №1 improved

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,26 +1,31 @@
 'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
     let totalAmount;
-    if (isNaN(parseInt(percent))) {
+    console.log(typeof date);
+    if (Number.isNaN(parseInt(percent)) || parseInt(percent) < 0) {
         return `Параметр percent содержит неправильное значение "${percent}"`;
     }
-    else if (isNaN(parseInt(contribution))) {
+    else if (Number.isNaN(parseInt(contribution)) || parseInt(contribution) < 0) {
         return `Параметр contribution содержит неправильное значение "${contribution}"`;
     }
-    else if (isNaN(parseInt(amount))) {
+    else if (Number.isNaN(parseInt(amount)) || parseInt(amount) < 0) {
         return `Параметр amount содержит неправильное значение "${amount}"`;
     }
-    else {
-        percent = parseInt(percent) / 1200;
-        contribution = parseInt(contribution);
-        amount = parseInt(amount);
-        let capital = amount - contribution;
-        let today = new Date();
-        let period = (date.getFullYear() - today.getFullYear()) * 12 - (date.getMonth() - today.getMonth());
-        let monthlyPay = capital * (percent + percent / (((1 + percent) ** period) - 1));
-        totalAmount = +((monthlyPay * period).toFixed(2));
-        console.log(totalAmount);
+    else if (Date.parse(date) !== Date.parse(date)) {
+        return 'Вы не заполнили параметр date';
     }
+    percent = parseInt(percent) / 1200;
+    contribution = parseInt(contribution);
+    amount = parseInt(amount);
+    let capital = amount - contribution;
+    let today = new Date();
+    let period = (date.getFullYear() - today.getFullYear()) * 12 - (date.getMonth() - today.getMonth());
+    if (period === 0) {
+        return 'Срок ипотеки должен составлять не менее одного месяца. Пожауста укажите правильный параметр period';
+    }
+    let monthlyPay = capital * (percent + percent / (((1 + percent) ** period) - 1));
+    totalAmount = +((monthlyPay * period).toFixed(2));
+    console.log(totalAmount);
     return totalAmount;
 }
 
