31f32e0dcc8ff6ee081e3463011306d318295528
Alexander Burachkov <zicio@yandex.ru>
2021-03-03 23:59:13 +0400

    2.1 Task №2 solved

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,4 +1,5 @@
 'use strict';
+//Задача №1
 function getSolutions(a, b, c) {
     let D = b ** 2 - 4 * a * c;
     let x1;
@@ -39,4 +40,45 @@ function showSolutionsMessage(a, b, c) {
     else {
         return `Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`;
     }
+}
+
+
+//Задача №2
+function getAverageScore(data) {
+    if (Object.keys(data).length === 0) {
+        data.average = 0;
+        return data;
+    }
+    else {
+        for (let prop in data) {
+            console.log(`Свойство ${prop}, значение: ${getAverageMark(data[prop])}`);
+            data[prop] = getAverageMark(data[prop]);
+        }
+
+        //Добавление доп.свойства average
+
+        //1 вариант  (как я понял reduce не работает при пустом объекте data)
+        /* data.average = (Object.values(data).reduce((a, b) => (a + b)) / (Object.values(data)).length);*/
+
+        //2 вариант
+        let sum1 = 0;
+        for (let i = 0; i < Object.values(data).length; i++)  {
+            sum1 += (Object.values(data))[i];
+        }
+        data.average = sum1 / Object.values(data).length;
+        return data;
+    }
+}
+
+function getAverageMark(marks) {
+    let sum = 0;
+    if (marks.length === 0) {
+        return sum;
+    }
+    else {
+        for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+        }
+        return sum / marks.length;
+    }  
 }
