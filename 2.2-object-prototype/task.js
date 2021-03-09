5d6351c36b331c66107fa0bf785920a7b6ad208e
Alexander Burachkov <zicio@yandex.ru>
2021-03-09 22:56:12 +0400

    2.2 Task №2, №3 Solved, Task №1 Fixed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,18 +1,27 @@
 'use strict'
-//String.prototype.isPalindrome - для задачи №1
-
+//Задача №1
 String.prototype.isPalindrome = function() {
-    //return this.toLowerCase().split("").reverse().join("") === this.toLowerCase();
     return this.replace(/ /g, '').toLowerCase().split("").reverse().join("") === this.replace(/ /g, '').toLowerCase();
 }
 
-
+//Задача №2
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+    if (marks.length === 0) {
+        return average;
+    }
+    else {
+        for (let i = 0; i < marks.length; i++) {
+            average += marks[i];
+        }
+        return Math.round(average / marks.length);
+    }
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = +new Date();
+    birthday = +new Date(birthday);
+    let diff = now - birthday;
+    let maturity = 31622400000/*кол-во миллисекунд в високосном году*/* 4 + 31536000000/*кол-во миллисекунд в обычном году*/ * 14;
+    return diff >= maturity;
 }
