e07a9f00cfe606da44fc3c2f6526900d1371a8b3
Бурачков Александр1 <BurachkovAA1@ssk63.ru>
2021-03-09 16:43:24 +0400

    2.2 Task №1 Solved

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,11 @@
+'use strict'
 //String.prototype.isPalindrome - для задачи №1
+
+String.prototype.isPalindrome = function() {
+    //return this.toLowerCase().split("").reverse().join("") === this.toLowerCase();
+    return this.replace(/ /g, '').toLowerCase().split("").reverse().join("") === this.replace(/ /g, '').toLowerCase();
+}
+
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
