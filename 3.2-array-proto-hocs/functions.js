e2164c1732d20f7736d42d1a9f13f8de001b7fb7
Alexander Burachkov <zicio@yandex.ru>
2021-03-18 00:07:32 +0400

    3.2.: Task №1 Solved

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -3,12 +3,37 @@
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 function getNames() {
     const nameWeapons = [];
-    weapons.forEach((item) => {
-        nameWeapons.push(item.name);
-    })
+    weapons.forEach(weapon => nameWeapons.push(weapon.name));
     return nameWeapons;
 } 
 
-function getCountReliableWeapons() {
-    weapons.indexOf(item.//weapons[1].durabulity)
+function collectReliableWeapons(durability) {
+    return weapons.filter(weapon => weapon.durability > durability);
+}
+
+function getCountReliableWeapons(durability) {
+    return collectReliableWeapons(durability).length;
+}
+
+function hasReliableWeapons(durability) {
+    return (weapons.some(weapon => weapon.durability > durability)); 
+}
+
+function getReliableWeaponsNames(durability) {
+    const nameReliableWeapons = [];
+    collectReliableWeapons(durability).forEach(weapon => nameReliableWeapons.push(weapon.name));
+    return nameReliableWeapons;
+}
+
+function getTotalDamage() {
+    return weapons.reduce((sum, current) => sum + current.attack, 0);
+}
+
+function getValuestCountToSumValues(arr, finalSum) {
+    const result = [];
+    arr.reduce(function (sum, current) {
+        result.push(sum + current);
+        return sum + current;
+    }, 0);
+    return result.indexOf(result.find(number => number >= finalSum)) + 1;
 }
